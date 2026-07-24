7dc5e1726a1137066d75d5d68edc3e663319f1b5
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-07-25 02:31:08 +0330

    Using new settings at /core

--- cli/cli.py
+++ cli/cli.py
@@ -11,7 +11,7 @@ from rich_gradient import Gradient  # Gradient effects in text output
 from termcolor import cprint  # Colored terminal printing
 
 # Import app settings
-from settings import settings
+from core.settings import settings
 
 # Initialize the rich console for pretty output
 console = Console()
