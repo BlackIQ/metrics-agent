27187114fca6542ff936215f7f90df9eb7d4c1e1
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-07-25 03:50:49 +0330

    Just a simple test to get data in response

--- routers/pull.py
+++ routers/pull.py
@@ -4,6 +4,14 @@ from fastapi import APIRouter, Depends
 # Application
 from dependencies.apikey import apikey_access  # API Key
 from dependencies.ip import ip_access  # IP
+from schemas.metrics import MetricsSchema
+
+# Collectors
+import collectors.system.collector as system
+import collectors.load.collector as load
+import collectors.memory.collector as memory
+import collectors.swap.collector as swap
+import collectors.cpu.collector as cpu
 
 # Router
 router = APIRouter(
@@ -12,6 +20,16 @@ router = APIRouter(
 )
 
 
-@router.get("/pull", dependencies=[Depends(ip_access), Depends(apikey_access)])
+@router.get(
+    "/pull",
+    response_model=MetricsSchema,
+    dependencies=[Depends(ip_access), Depends(apikey_access)],
+)
 async def Pull():
-    return {"message": "Route: /api/pull"}
+    return MetricsSchema(
+        system=system.collect(),
+        load=load.collect(),
+        memory=memory.collect(),
+        swap=swap.collect(),
+        cpu=cpu.collect(),
+    )
