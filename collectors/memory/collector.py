95a5750d4760710f705aae788de025797f070da5
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-07-25 07:20:19 +0330

    Class plugin: Memory

--- collectors/memory/collector.py
+++ collectors/memory/collector.py
@@ -1,20 +1,44 @@
-# Schema
-from .schema import CollectorSchema
+# Asyncio
+import asyncio
 
 # PS Util
 import psutil
 
+# Schema
+from .schema import CollectorSchema
 
-def collect() -> CollectorSchema:
-    memory = psutil.virtual_memory()
+# Application
+from services.save import save as save_metric  # Save Metrics
 
-    return CollectorSchema(
-        total=memory.total,
-        active=memory.active,
-        inactive=memory.inactive,
-        free=memory.free,
-        used=memory.used,
-        available=memory.available,
-        percent=memory.percent,
-        wired=memory.wired,
-    )
+
+class Collector:
+    name = "memory"
+
+    interval = 5
+
+    schema = CollectorSchema
+
+    def collect(self) -> CollectorSchema:
+        memory = psutil.virtual_memory()
+
+        return CollectorSchema(
+            total=memory.total,
+            active=memory.active,
+            inactive=memory.inactive,
+            free=memory.free,
+            used=memory.used,
+            available=memory.available,
+            percent=memory.percent,
+            wired=memory.wired,
+        )
+
+    async def save(self, metrics: CollectorSchema):
+        await save_metric(self.name, metrics)
+
+    async def run(self):
+        while True:
+            metrics = self.collect()
+
+            await self.save(metrics)
+
+            await asyncio.sleep(self.interval)
