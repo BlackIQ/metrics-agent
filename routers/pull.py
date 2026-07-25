d4482cd0a42d8c43ef0823a8671610908b8f1c79
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-07-25 07:21:42 +0330

    Remove that thing

--- routers/pull.py
+++ routers/pull.py
@@ -4,14 +4,6 @@ from fastapi import APIRouter, Depends
 # Application
 from dependencies.apikey import apikey_access  # API Key
 from dependencies.ip import ip_access  # IP
-from schemas.metrics import MetricsSchema
-
-# Collectors
-import collectors.system.collector as system
-import collectors.load.collector as load
-import collectors.memory.collector as memory
-import collectors.swap.collector as swap
-import collectors.cpu.collector as cpu
 
 # Router
 router = APIRouter(
@@ -20,16 +12,6 @@ router = APIRouter(
 )
 
 
-@router.get(
-    "/pull",
-    response_model=MetricsSchema,
-    dependencies=[Depends(ip_access), Depends(apikey_access)],
-)
+@router.get("/pull", dependencies=[Depends(ip_access), Depends(apikey_access)])
 async def Pull():
-    return MetricsSchema(
-        system=system.collect(),
-        load=load.collect(),
-        memory=memory.collect(),
-        swap=swap.collect(),
-        cpu=cpu.collect(),
-    )
+    return True
