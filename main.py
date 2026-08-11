0c35a79c4748efadb05d7573ecc6df4089df1397
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-08-12 01:13:38 +0330

    Test for I/O builtin plugin

--- main.py
+++ main.py
@@ -27,7 +27,8 @@ async def lifespan(app: FastAPI):
         # {"name": "swap", "enabled": True, "options": {"interval": 5}},
         # {"name": "load", "enabled": True, "options": {"interval": 5}},
         # {"name": "network", "enabled": True, "options": {"interval": 5}},
-        {"name": "disk", "enabled": True, "options": {"interval": 10}},
+        # {"name": "disk", "enabled": True, "options": {"interval": 10}},
+        {"name": "io", "enabled": True, "options": {"interval": 5}},
     ]
 
     await collector_manager.reload_config(default_config)
