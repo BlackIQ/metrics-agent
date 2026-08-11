5c7b848ea22050e13303fbeca2722fea09232996
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-08-12 01:56:51 +0330

    Add logger to pull router. Issue: https://github.com/OpenHubble/metrics-agent/issues/9

--- routers/pull.py
+++ routers/pull.py
@@ -9,6 +9,7 @@ from sqlalchemy import select
 from datetime import datetime, timezone
 
 # Application
+from core.logger import get_logger  # Logger
 from dependencies.apikey import apikey_access
 from dependencies.ip import ip_access
 from dependencies.database import get_db
@@ -16,6 +17,10 @@ from enums.sync import SyncStatus
 from schemas.metrics import ReadMetricsSchema, AckSchema
 from models import Metric
 
+# Logger (Pull)
+logger = get_logger("router.pull")
+
+# Router
 router = APIRouter(
     prefix="",
     tags=["Pull"],
@@ -51,6 +56,8 @@ async def pull_metrics(
 
     db.commit()
 
+    logger.info(f"Dispatched {len(db_metrics)} metrics to cloud pull client.")
+
     return db_metrics
 
 
@@ -69,6 +76,10 @@ async def acknowledge_metrics(
     db_metrics = db.scalars(stmt).all()
 
     if not db_metrics:
+        logger.warning(
+            f"ACK failed: No metrics found matching IDs {payload.metric_ids}"
+        )
+
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="No matching metric records found for given IDs",
@@ -79,5 +90,7 @@ async def acknowledge_metrics(
         metric.synced_at = now
 
     db.commit()
+
+    logger.info(f"Acknowledged {len(db_metrics)} metrics.")
 
     return {"message": f"Successfully acknowledged {len(db_metrics)} metrics"}
