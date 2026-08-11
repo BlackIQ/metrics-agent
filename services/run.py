15dbe62cd4990474250def74f59e0c28d2420ba8
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-08-12 00:34:03 +0330

    Just edit path

--- services/run.py
+++ services/run.py
@@ -2,11 +2,11 @@
 import asyncio
 
 # Collectors
-from collectors.system.collector import Collector as SystemCollector
-from collectors.load.collector import Collector as LoadCollector
-from collectors.processor.collector import Collector as ProcessorCollector
-from collectors.memory.collector import Collector as MemoryCollector
-from collectors.swap.collector import Collector as SwapCollector
+from collectors.builtin.system.collector import Collector as SystemCollector
+from collectors.builtin.load.collector import Collector as LoadCollector
+from collectors.builtin.processor.collector import Collector as ProcessorCollector
+from collectors.builtin.memory.collector import Collector as MemoryCollector
+from collectors.builtin.swap.collector import Collector as SwapCollector
 
 
 async def start_collectors():
